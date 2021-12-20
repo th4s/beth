a902ae6258acc3ac13a9fbb962372daf71257352
th4s <th4s@metavoid.xyz>
2021-12-20 18:27:09 +0100

    WIP: Adding more functions...

--- beth.sh
+++ beth.sh
@@ -2,28 +2,57 @@ if [ ! -x "$(command -v curl)" ]; then
     echo "Please install curl to use this script." && return
 fi
 
+set -o pipefail
+
 DEFAULT_ETH_RPC_URL="https://cloudflare-eth.com"
 
 if [ -z "${ETH_RPC_URL}" ]; then
     ETH_RPC_URL=${DEFAULT_ETH_RPC_URL}
 fi
+
+eth_syncing() {
+    __request "eth_syncing"
+}
     
+eth_mining() {
+    __request "eth_mining"
+}
+    
+eth_hashrate() {
+    __request "eth_hashrate" | __hex_to_dec
+}
+
 eth_gas_price() {
-    __hex_to_dec $(__parse_result $(__request eth_gasPrice))
+    __request "eth_gasPrice" | __hex_to_dec
+}
+
+eth_accounts() {
+    __request "eth_accounts"
+}
+
+eth_block_number() {
+    __request "eth_blockNumber" | __hex_to_dec
 }
 
 __request() {
-    curl ${ETH_RPC_URL} -sH "Content-Type: application/json" \
-        -d "{\"jsonrpc\": \"2.0\",\"method\": \"${1}\",\"params\": [${@:2}],\"id\": ${RANDOM}}"
-}
-
-
-__parse_result() {
-    sed -e 's/.*"result": *"\(.*\)".*/\1/' <<<"${1}"
+    curlArgs=('-H' "Content-Type: application/json")
+    if [ ! -z "${ETH_RPC_AUTH_BASIC}" ]; then
+        curlArgs+=('-H' "Authorization: Basic ${ETH_RPC_AUTH_BASIC}")
+    elif [ ! -z "${ETH_RPC_AUTH_BEARER}" ]; then
+        curlArgs+=('-H' "Authorization: Bearer ${ETH_RPC_AUTH_BEARER}")
+    fi
+    out=$(curl ${ETH_RPC_URL} -s "${curlArgs[@]}" \
+        -d "{\"jsonrpc\": \"2.0\",\"method\": \"${1}\",\"params\": [${@:2}],\"id\": ${RANDOM}}")
+    if [[ $out == *result* ]]; then
+        sed -e 's/.*"result": *\(.*\)}.*/\1/' <<<"${out}" | sed -e 's/\"//g'
+    else
+        echo $out
+    fi
 }
 
 __hex_to_dec() {
-   printf "%d\n" ${1}
+    read value <<<$(cat)
+    printf "%d\n" ${value}
 }
 
 gwei() {
